a0bd87c41e08bafcbc79929680a90058bc94ae96
mariozapatero <mario_zc@hotmail.com>
2022-03-05 21:28:34 +0100

    actualizado

---
+++ src/text_functions.py
@@ -0,0 +1,11 @@
+# Lista de palabras relacionadas con el atributo de marca 'precio'.
+
+price_list = ['ackers', 'affordable', 'banknote', 'bargain', 'bill', 'bill', 'boodle', 'brass', 'brass', 'bread',
+              'buck', 'budget', 'capital', 'carfare', 'cash', 'cent', 'charge', 'cheap', 'coin', 'competitive', 'copper',
+              'cost', 'cost', 'currency', 'damage', 'dibs', 'dime', 'dinero', 'discount', 'dollar', 'dosh', 'dough',
+              'ducats', 'economic', 'economy', 'euros', 'expenditure', 'expense', 'expensive', 'fare', 'fee', 'fraud',
+              'funds', 'gelt', 'gold', 'gravy', 'greenbacks', 'invoice', 'levy', 'lolly', 'loot', 'lucre', 'mazuma',
+              'means', 'monetary', 'money', 'money', 'moolah', 'note', 'noting', 'oof', 'outlay', 'pay', 'pence', 'penny',
+              'plonk', 'plunder', 'plunk', 'price', 'pricy', 'quotation', 'rate', 'rhino', 'rich', 'rob', 'sale',
+              'settlement', 'shekels', 'silver', 'simoleons', 'smacker', 'splosh', 'spondulicks', 'steal', 'sterling',
+              'sum', 'terms', 'theft', 'thievery', 'toll', 'valuation', 'value', 'wherewithal', 'wonga', 'worthy']
